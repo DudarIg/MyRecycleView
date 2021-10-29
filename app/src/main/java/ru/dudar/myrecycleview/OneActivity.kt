724c3b9aa550622e8c6403e0f35c29f71d56a955
pakad <pakad@mail.ru>
2021-10-29 11:06:01 +0300

    Добавил пакет tools.kt

--- app/src/main/java/ru/dudar/myrecycleview/OneActivity.kt
+++ app/src/main/java/ru/dudar/myrecycleview/OneActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import java.text.SimpleDateFormat
-import java.util.*
 
 private const val EXTRA_NAME = "extra_name"
 private const val EXTRA_COLOR = "extra_color"

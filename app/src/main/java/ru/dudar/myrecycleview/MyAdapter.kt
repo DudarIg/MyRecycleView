354ce41629d7fd2fc0401d25be0e2c192f71cf0d
pakad <pakad@mail.ru>
2021-10-29 10:25:49 +0300

    Добавил activity_one_constr.xml

--- app/src/main/java/ru/dudar/myrecycleview/MyAdapter.kt
+++ app/src/main/java/ru/dudar/myrecycleview/MyAdapter.kt
@@ -1,20 +1,16 @@
 package ru.dudar.myrecycleview
 
-import android.annotation.SuppressLint
+
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import java.security.AccessController.getContext
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MyAdapter(val cats: List<Cat>, val myContext: Context):
+class MyAdapter(private val cats: List<Cat>, private val myContext: Context):
                                 RecyclerView.Adapter<MyAdapter.MyHolder>() {
 
     class MyHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -30,7 +26,7 @@ class MyAdapter(val cats: List<Cat>, val myContext: Context):
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         val cat = cats[position]
         holder.text1.text = cat.name
-        val age = "${cat.age.toString()} ${god(cat.age)}"
+        val age = "${cat.age} ${god(cat.age)}"
         holder.itemView.setOnClickListener {
             val intent = OneActivity.newIntent(myContext, cat.name, cat.color, age, getDateToString(cat.date) )
             myContext.startActivity(intent)
@@ -41,7 +37,7 @@ class MyAdapter(val cats: List<Cat>, val myContext: Context):
         return cats.size
     }
 
-    fun god(g : Int) : String {
+    private fun god(g : Int) : String {
         when (g) {
             1 ->  return "год"
             in 2..4 -> return  "года"
@@ -50,7 +46,7 @@ class MyAdapter(val cats: List<Cat>, val myContext: Context):
         return "нет данных"
     }
 
-    fun getDateToString(data : Date): String{
+    private fun getDateToString(data : Date): String{
         //val dtime = Calendar.getInstance().time
         val formatter = SimpleDateFormat("dd-MM-yy kk:mm", Locale.getDefault())
         return formatter.format(data)
